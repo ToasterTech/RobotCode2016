9449b2f41136daa3a9a25502e1365b7bb97d1249
Tyler Allen <tallen40@gatech.edu>
2016-02-12 17:23:59 -0500

    Added some example classes for the drive to show how the subsystem stack works. I will put more documentation in later.

---
+++ src/org/usfirst/frc/team5332/robot/drive/DriveCommandLayer.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team5332.robot.drive;
+
+import subsystem.Layer;
+
+public class DriveCommandLayer implements Layer<DriveSytemLayer>{
+
+	@Override
+	public void runPeriodic() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setChild(DriveSytemLayer c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/org/usfirst/frc/team5332/robot/drive/DriveSytemLayer.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team5332.robot.drive;
+
+import subsystem.Layer;
+
+public class DriveSytemLayer implements Layer<DriveHardwareLayer>{
+
+	@Override
+	public void runPeriodic() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setChild(DriveHardwareLayer c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
