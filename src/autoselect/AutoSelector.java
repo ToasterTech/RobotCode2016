e9828e85314e30235dad224c040052562d6fb8b2
Tyler Allen <interfior99@gmail.com>
2016-02-19 21:32:27 -0500

    Added more auto mode CommandLayers. No implementation yet.
    Added the auto selector module. Backend implmented on a hashtable.

---
+++ src/autoselect/AutoSelector.java
@@ -0,0 +1,38 @@
+package autoselect;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.usfirst.frc.team5332.robot.drive.DriveAutoHingedRamps;
+import org.usfirst.frc.team5332.robot.drive.DriveAutoLowBar;
+import org.usfirst.frc.team5332.robot.drive.DriveAutoRoughTerrain;
+import org.usfirst.frc.team5332.robot.drive.base.DriveCommandLayer;
+import org.usfirst.frc.team5332.robot.intake.base.IntakeCommandLayer;
+
+public class AutoSelector {
+	private ArrayList<DriveCommandLayer> driveAutoList;
+	private ArrayList<IntakeCommandLayer> intakeAutoList;
+	private HashMap<String,DriveCommandLayer> driveAutoMap;
+	public AutoSelector (){
+//		driveAutoList = new ArrayList<DriveCommandLayer>();
+//		intakeAutoList = new ArrayList<IntakeCommandLayer>();
+		driveAutoMap=new HashMap<String,DriveCommandLayer>();
+//		driveAutoList.add(new DriveAutoLowBar());
+//		driveAutoList.add(new DriveAutoRoughTerrain());
+//		driveAutoList.add(new DriveAutoHingedRamps());
+		DriveCommandLayer auto=new DriveAutoLowBar();
+		driveAutoMap.put(auto.getName(), auto);
+		auto=new DriveAutoHingedRamps();
+		driveAutoMap.put(auto.getName(), auto);
+		auto=new DriveAutoRoughTerrain();
+		driveAutoMap.put(auto.getName(), auto);
+
+		
+	}
+	public IntakeCommandLayer getIntakeAuto(String input){
+		return null;
+	}
+	public DriveCommandLayer getDriveAuto(String input){
+		return driveAutoMap.get(input);
+	}
+}
---
+++ src/org/usfirst/frc/team5332/robot/drive/DriveAutoLowBar.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team5332.robot.drive;
+
+import org.usfirst.frc.team5332.robot.drive.base.DriveCommandLayer;
+import org.usfirst.frc.team5332.robot.drive.base.DriveSystemLayer;
+
+public class DriveAutoLowBar extends DriveCommandLayer {
+	@Override
+	public void robotInit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runPeriodic() {
+		// TODO Auto-generated method stub
+		systemLayer.setDriveHardwareLeft(1);
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "LowBar";
+	}
+
+}
